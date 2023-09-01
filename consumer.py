300d46e203f1b4e7f5495d9a27cf192b7c2570ca
Huy <dnnhhuy@Huy-MacBook-Pro.local>
2023-09-02 01:55:08 +0700

    Add cassandra setup cql

--- consumer.py
+++ consumer.py
@@ -2,7 +2,7 @@ from pyspark.sql import SparkSession
 from schema import schema
 from pyspark.sql import functions as func
 from pyspark.sql.types import *
-
+from pyspark import SparkConf
 
 def fix_string(str):
     if str: 
@@ -11,10 +11,8 @@ def fix_string(str):
             .encode("latin-1") \
             .decode("utf-8") \
             .strip('\"')
-    
     return str
     
-
 
 def readstream_from_kafka(spark, topic):
     data = spark.readStream \
@@ -33,7 +31,12 @@ def transform_data(data, topic):
         .select(func.from_json(func.col("value"), schema[topic]).alias("value")) \
         .select(func.col("value.*")) \
         .withColumn("ts", (func.col("ts")/1000).cast("timestamp")) \
-        .withColumn("registration", (func.col("registration")/1000).cast("timestamp"))
+        .withColumn("registration", (func.col("registration")/1000).cast("timestamp")) \
+        .withColumn("day", func.dayofmonth(func.col("ts"))) \
+        .withColumn("month", func.month(func.col("ts"))) \
+        .withColumn("year", func.year(func.col("ts"))) \
+        .withColumn("hour", func.hour(func.col("ts"))) \
+        .withColumn("week", func.weekofyear(func.col("ts")))
     
     if topic == "listen_events" or topic == "page_view_events":
         transformed_data = transformed_data.withColumn("song", convertudf(func.col("song"))) \
@@ -49,57 +52,73 @@ def write_to_hdfs(data, topic):
         .option("path", "hdfs://namenode:8020/data/{}".format(topic)) \
         .option("checkpointLocation", "hdfs://namenode:8020/checkpoint/{}".format(topic)) \
         .trigger(processingTime="1 minutes")
-        
     return stream
+        
 
 
 def write_to_cassandra(data, topic):
-    data.writeStream \
+    stream = data.writeStream \
         .format("org.apache.spark.sql.cassandra") \
         .outputMode("append") \
         .options(table=topic, keyspace="music_streaming") \
         .option("checkpointLocation", "/tmp/checkpoint/{}".format(topic)) \
-        .start()
         
-if __name__ == '__main__':
+    return stream
+
+def initialize_spark():
+    conf = SparkConf()
+    conf.set("spark.cores.max", 2)
+    conf.set("spark.executor.memory", "4g")
+    conf.set("spark.driver.memory", "4g")
+    conf.set("spark.cassandra.connection.host", "cassandra")
+    conf.set("spark.cassandra.connection.port","9042")
+    # conf.set("spark.cassandra.auth.username", "cassandra")
+    # conf.set("spark.cassandra.auth.password", "cassandra")
     
     spark = SparkSession.builder \
         .master("spark://spark-master:7077") \
-        .config("spark.cores.max", 1) \
-        .config("spark.executor.memory", "4g") \
-        .config("spark.driver.memory", "4g") \
-        .config("spark.cassandra.connection.host", "cassandra") \
-        .config("spark.cassandra.connection.port","9042") \
-        .config("spark.cassandra.auth.username", "cassandra") \
-        .config("spark.cassandra.auth.password", "cassandra") \
+        .config(conf=conf) \
         .appName("KafkaConsumer").getOrCreate()
+        
+    return spark
 
+
+if __name__ == '__main__':
+    
+    # Initialize Spark
+    spark = initialize_spark()
+    
     spark.sparkContext.setLogLevel("ERROR")
     
     # Process Stream Listen Events
     listen_events = readstream_from_kafka(spark, "listen_events")
     listen_events = transform_data(listen_events, "listen_events")
-    write_to_cassandra(listen_events, "listen_events")
-    listen_events = write_to_hdfs(listen_events, "listen_events")
-    
-    
-    
+    listen_events_cassandra = write_to_cassandra(listen_events, "listen_events")
+    listen_events_hdfs = write_to_hdfs(listen_events, "listen_events")
     
     # Process Stream Auth Events
     auth_events = readstream_from_kafka(spark, "auth_events")
     auth_events = transform_data(auth_events, "auth_events")
-    auth_events = write_to_hdfs(auth_events, "auth_events")
+    auth_events_cassandra = write_to_cassandra(auth_events, "auth_events")
+    auth_events_hdfs = write_to_hdfs(auth_events, "auth_events")
     
     # Process Stream Page View Events
     page_view_events = readstream_from_kafka(spark, "page_view_events")
     page_view_events = transform_data(page_view_events, "page_view_events")
-    page_view_events = write_to_hdfs(page_view_events, "page_view_events")
+    page_view_events_cassandra = write_to_cassandra(page_view_events, "page_view_events")
+    page_view_events_hdfs = write_to_hdfs(page_view_events, "page_view_events")
     
-    auth_events.start()
-    listen_events.start()
-    page_view_events.start()
+    listen_events_cassandra.start()
+    # listen_events_hdfs.start()
+    
+    auth_events_cassandra.start()
+    # auth_events_hdfs.start()
+    
+    page_view_events_cassandra.start()
+    # page_view_events_hdfs.start()
     
     spark.streams.awaitAnyTermination()
 
+    spark.stop()
 
     
