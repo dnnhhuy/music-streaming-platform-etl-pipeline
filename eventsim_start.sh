4d0ffdf891e7eddcf60baa449754605acd21f7dd
Huy <dnnhhuy@Huy-MacBook-Pro.local>
2023-09-20 01:32:27 +0700

    transform user data

--- eventsim_start.sh
+++ eventsim_start.sh
@@ -15,4 +15,4 @@ docker run -itd \
     --continuous
 
   
-docker exec spark-master spark-submit --packages org.apache.spark:spark-sql-kafka-0-10_2.12:3.3.0,com.datastax.spark:spark-cassandra-connector_2.12:3.3.0 /src/consumer.py
+docker exec spark-master spark-submit --packages org.apache.spark:spark-sql-kafka-0-10_2.12:3.3.0,com.datastax.spark:spark-cassandra-connector_2.12:3.3.0 /src/stream_process.py
---
+++ spark/batch_process.py
@@ -0,0 +1,22 @@
+from pyspark.sql import SparkSession
+from pyspark.sql.types import *
+from pyspark.sql import functions as func
+
+spark = SparkSession.builder \
+                    .master("spark://spark-master:7077") \
+                    .config("spark.cores.max", 1) \
+                    .config("spark.executor.memory", "4g") \
+                    .config("spark.driver.memory", "4g") \
+                    .appName("BatchProcess") \
+                    .getOrCreate()
+                    
+
+listen_events_df = spark.read \
+                    .format("parquet") \
+                    .load("hdfs://namenode:8020/data/listen_events")
+
+listen_events_df = listen_events_df.groupBy("song").count().sort(func.desc(func.col("count")))
+
+listen_events_df.show()
+
+spark.stop()
